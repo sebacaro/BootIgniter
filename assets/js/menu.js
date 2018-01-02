9165f0fb34d89ca5d3023481799477f92928cf20
Seba <sebacaro@gmail.com>
2018-01-02 12:08:06 -0300

    Agregar app del banco

---
+++ assets/js/menu.js
@@ -0,0 +1,76 @@
+
+
+/* Aquí pueden administrar el destino de sus rutas en el menu*/
+
+document.write('<ul id="sidebar-nav" class="sidebar-nav margin-bottom-30px">');
+
+document.write('	<li class="list-group-item has-submenu ">');
+document.write('		<a href="#Buttons" class="submenu-toggle" data-toggle="collapse" data-parent="sidebar-nav">Botones</a>');
+document.write('		<ul id="Buttons" class="list-unstyled collapse submenu ">');
+document.write('			<li><a href="buttons.html">Botones</a></li>');
+document.write('		</ul>');
+document.write('	</li>');
+
+document.write('	<li class="list-group-item has-submenu">');
+document.write('		<a href="#Feedback" class="submenu-toggle" data-toggle="collapse" data-parent="sidebar-nav">Feedback y mensajería</a>');
+document.write('		<ul id="Feedback" class="list-unstyled collapse submenu">');
+document.write('			<li><a href="feedback-popover.html">Popover</a></li>');
+document.write('			<li><a href="feedback-spinner.html">Spinner</a></li>');
+document.write('			<li><a href="feedback-modal.html">Modal</a></li>');
+document.write('			<li><a href="feedback-receipts.html">Comprobante finalización</a></li>');
+document.write('		</ul>');
+document.write('	</li>');
+
+document.write('    <li class="list-group-item has-submenu">');
+document.write('        <a href="#Forms" class="submenu-toggle" data-toggle="collapse" data-parent="sidebar-nav">Formularios</a>');
+document.write('        <ul id="Forms" class="list-unstyled collapse submenu">');
+document.write('            <li><a href="form-inputs.html">Inputs</a></li>');
+document.write('            <li><a href="form-security.html">Token y coordenadas</a></li>');
+document.write('            <li><a href="form-label.html">Labels</a></li>');
+document.write('            <li><a href="form-textarea.html">Text area</a></li>');
+document.write('            <li><a href="form-select.html">Select</a></li>');
+document.write('            <li><a href="form-check.html">Checkbox</a></li>');
+document.write('            <li><a href="form-radio.html">Radio buttons</a></li>');
+document.write('            <li><a href="form-tables.html">Tablas</a></li>');
+document.write('        </ul>');
+document.write('    </li>');
+
+document.write('	<li class="list-group-item has-submenu">');
+document.write('		<a href="#Layout" class="submenu-toggle" data-toggle="collapse" data-parent="sidebar-nav">Layout</a>');
+document.write('		<ul id="Layout" class="list-unstyled collapse submenu">');
+document.write('			<li><a href="layout-contenedor.html">Contenedor</a></li>');
+document.write('			<li><a href="layout-lists.html">Listas</a></li>');
+document.write('			<li><a href="headers.html">Header mobile</a></li>');
+document.write('			<li><a href="menu_aside.html">Menú lateral</a></li>');
+document.write('			<li><a href="layout-splash-screen.html">Splash screen</a></li>');
+document.write('		</ul>');
+document.write('	</li>');
+
+document.write('	<li class="list-group-item has-submenu">');
+document.write('		<a href="#Content" class="submenu-toggle" data-toggle="collapse" data-parent="sidebar-nav">Organizadores de contenido</a>');
+document.write('		<ul id="Content" class="list-unstyled collapse submenu">');
+document.write('			<li><a href="organizadores-accordion.html">Accordion</a></li>');
+document.write('			<li><a href="organizadores-tabs.html">Tabs</a></li>');
+document.write('			<li><a href="organizadores-slideshow.html">Slideshow</a></li>');
+document.write('			<li><a href="organizadores-stepleft.html">Barra de pasos (step left)</a></li>');
+document.write('		</ul>');
+document.write('	</li>');
+
+document.write('	<li class="list-group-item has-submenu">');
+document.write('		<a href="#Text" class="submenu-toggle" data-toggle="collapse" data-parent="sidebar-nav">Textos, colores, íconos</a>');
+document.write('		<ul id="Text" class="list-unstyled collapse submenu">');
+document.write('			<li><a href="text.html">Textos</a></li>');
+document.write('			<li><a href="colors.html">Colores</a></li>');
+document.write('			<li><a href="icons.html">Iconos</a></li>');
+document.write('		</ul>');
+document.write('	</li>');
+
+document.write('	<li class="list-group-item has-submenu">');
+document.write('		<a href="#Otros" class="submenu-toggle" data-toggle="collapse" data-parent="sidebar-nav">Otros componentes</a>');
+document.write('		<ul id="Otros" class="list-unstyled collapse submenu">');
+document.write('			<li><a href="filtros.html">Filtros</a></li>');
+document.write('		</ul>');
+document.write('	</li>');
+
+document.write('</ul>');
+
